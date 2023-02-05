34cba62e9a1f1217e4eb7bfc2b9c2ee74f3832c0
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-05 15:59:53 -0500

    Implment controller for new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -5,6 +5,27 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Product.new
   end
+
+  def create
+    @category = Product.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name
+    )
+  end
+
+
 
 end
