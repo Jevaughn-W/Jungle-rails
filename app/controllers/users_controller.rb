246c92a48218c65daa60310dcdc5c199e087e813
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-06 21:29:47 -0500

    Add missing params.require method to user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params(:user).permit(
+  def user_params
+    params.require(:user).permit(
     :first_name,
     :last_name,
     :email,
