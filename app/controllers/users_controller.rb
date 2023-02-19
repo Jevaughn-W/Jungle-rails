a1e5e7b585b42b5535aa4b9d362352f07d44cfb0
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-19 18:49:32 -0500

    Update new controller to instantiate a new object, and update to process error message is validation is not met

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to "/"
     else
-      redirect_to "/signup"
+      render :new, status: :unprocessable_entity
     end
   end
 
