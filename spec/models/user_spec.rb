74a35e74fded377e2eae125da47830a3c4b86803
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-19 20:24:12 -0500

    Add tests for user authentication class method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -128,8 +128,58 @@ RSpec.describe User, type: :model do
       }.to raise_error(ActiveRecord::RecordInvalid)
     
     end
+  end
 
+  describe ".authenticate_with_credentials" do
+    it "should not create a user if their email does not exist" do
+    
+      expect(User.authenticate_with_credentials("jackl@yahoo.com", "lol")).to be nil
+    end
 
+    it "should not create a user if their password does not match" do
+      
+      @user = User.new({
+          first_name: "Test First Name",
+          last_name: "Test Last Name",
+          email: "testfirst_testlast@test.com",
+          password: "tester1",
+          password_confirmation: "tester1"
+        })
+
+        @user.save!
+
+      expect(User.authenticate_with_credentials("testfirst_testlast@test.com", "lol")).to be nil
+    end
+
+    it "should create a user if their email and password match" do
+      
+      @user = User.new({
+          first_name: "Test First Name",
+          last_name: "Test Last Name",
+          email: "testfirst_testlast@test.com",
+          password: "tester1",
+          password_confirmation: "tester1"
+        })
+
+        @user.save!
+
+      expect(User.authenticate_with_credentials("testfirst_testlast@test.com", "tester1")).to be_present
+    end
+
+    it "should create a user if there are spaces in around thier email" do
+      
+      @user = User.new({
+          first_name: "Test First Name",
+          last_name: "Test Last Name",
+          email: "testfirst_testlast@test.com",
+          password: "tester1",
+          password_confirmation: "tester1"
+        })
+
+        @user.save!
+
+      expect(User.authenticate_with_credentials("  testfirst_testlast@test.com", "tester1")).to be_present
+    end
   end
 
 end
