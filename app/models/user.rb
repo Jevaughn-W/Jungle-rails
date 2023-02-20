a08fbe72b13420f8eb7155e48e3cd0b741a2d015
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-19 20:31:17 -0500

    Add feature to check and remove leading and trailing spaces from email and make email check indifferent of casing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    @user = self.find_by_email(email)
+    @user = self.find_by_email(email.strip.downcase)
 
     if @user && @user.authenticate(password)
       @user
