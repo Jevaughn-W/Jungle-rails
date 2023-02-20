c458ddad30213c696691ed90fa093a0bc3a28800
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-19 20:23:02 -0500

    Add class method to validate user credentials for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    @user = self.find_by_email(email)
+
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
 end
